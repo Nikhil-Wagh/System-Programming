f63829c81a226ea0405a5041ad82afe0f9a61d3e
Nikhil Wagh <nikhilwagh.work@gmail.com>
2018-04-11 02:40:07 +0530

    added files

--- Lexical Analyser/analyser.cpp
+++ Lexical Analyser/analyser.cpp
@@ -1,13 +1,15 @@
 #include "bits/stdc++.h"
 using namespace std;
 #define delCount 15
+#define keyCount 22
 
 string tokens[100];
+char delimiters[100];
 
 void init(){
 	for(int i = 0 ; i < delCount ; i++)
 		cin >> delimiters[i];
-	for(int i = 0 ; i < tokenCount ; i++)
+	for(int i = 0 ; i < keyCount ; i++)
 		cin >> tokens[i];
 }
 
@@ -17,6 +19,13 @@ bool isDelimiter(char s){
 			return true;
 	}
 	return false;
+}
+
+bool isKeyword(string s){
+    for(int i = 0 ; i < keyCount ; i++)
+        if(s == tokens[i])
+            return true;
+    return false;
 }
 
 int main(int argc, char const *argv[])
@@ -35,12 +44,17 @@ int main(int argc, char const *argv[])
   		bool inQuotes = false;
   		while(right < length && left <= right){
   			line.erase(remove(line.begin(), line.end(), '\t'), line.end());
-  			if((isDelimiter(line[right]) || line[right] == ' ') && inQuotes == false){
+  			if((isDelimiter(line[right]) || line[right] == ' ') && (!inQuotes)){
   				if(left != right){
-  					cout << line.substr(left, right - left) << endl;
+                    string temp = line.substr(left, right - left);
+                    cout << temp;
+                    if(isKeyword(temp))
+                        cout << " is a KEYWORD" <<  endl;
+                    else
+                        cout << " is an IDENTIFIER" << endl;
   				}
   				if(line[right] != ' ')
-  					cout << line[right] << endl;
+  					cout << line[right] << " is an OPERATOR" << endl;
   				left = right + 1;
   			}
   			else{
